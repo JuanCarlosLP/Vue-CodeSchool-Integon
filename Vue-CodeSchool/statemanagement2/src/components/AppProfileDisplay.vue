786db3a858aa624c28a1ce16802c98c106b33f99
Juan Carlos Larios Pinto <48814846+JuanCarlosLP@users.noreply.github.com>
2021-09-17 11:16:12 -0500

    Add files via upload

---
+++ Vue-CodeSchool/statemanagement2/src/components/AppProfileDisplay.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="md:w-1/3 flex flex-col p-12">
+  <!-- Profile Card -->
+    <h3 class="font-bold font-lg">{{ formData.name }}</h3>
+    <p class="mt-2">{{ formData.occupation }}</p>
+  </section>
+</template>
+
+<script>
+import eventBus from '../event-bus';  // Importamos al Bus de Eventos
+export default {
+  mounted() {
+    eventBus.$on('profileUpdate', this.update)  //EventBus avisa cuando haya un ProfileUpdate
+  },
+  beforeDestroy() {
+    eventBus.$off('profileUpdate', this.update)
+  },
+  methods: {
+    update(formData) {
+      this.formData = formData;
+    }
+  },
+  data () {
+    return {
+      formData: {}
+    }
+  }
+  // props: {                 // Ya no usamos prop porque la info ya no viene del padre
+  //   formData: {
+  //     type: Object,
+  //     default() {
+  //       return {}
+  //     }
+  //   }
+  // }
+}
+</script>
---
+++ Vue-CodeSchool/vue-activity01/src/App.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="container">
+    <h2>Shopping list</h2>
+    <div class="user-input">
+      <input
+        placeholder="Press enter to add new item"
+        v-model="newItem"
+      /><button @click="addItem()">Add item</button>
+    </div>
+
+    <ul>
+      <li class="item" v-for="(item, n) in shoppingList" :key="n">
+        {{ item }}
+        <a class="button--remove" @click="deleteItem(item)">Remove</a>
+      </li>
+    </ul>
+
+    <button class="button--delete" @click="removeAll()">Remove All</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newItem: '',
+      shoppingList: [],
+    }
+  },
+  methods: {
+    addItem() {
+      this.shoppingList.push(this.newItem)
+    },
+    deleteItem(value) {
+      this.shoppingList = this.shoppingList.filter(item => item !== value)
+    },
+    removeAll(){
+      this.shoppingList = [];
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import 'styles/global';
+
+$color-green: #4fc08d;
+$color-grey: #2c3e50;
+
+.container {
+  max-width: 600px;
+  margin: 80px auto;
+}
+
+.item {
+  display: flex;
+  justify-content: space-between;
+}
+
+// Type
+.h2 {
+  font-size: 21px;
+}
+
+.user-input {
+  display: flex;
+  align-items: center;
+  padding-bottom: 20px;
+  input {
+    width: 100%;
+    padding: 10px 6px;
+    margin-right: 10px;
+  }
+}
+
+.item {
+  display: flex;
+  align-items: center;
+}
+
+// Buttons
+button {
+  appearance: none;
+  padding: 10px;
+
+  font-weight: bold;
+  border-radius: 10px;
+  border: none;
+  background: $color-grey;
+  color: white;
+  white-space: nowrap;
+
+  + button {
+    margin-left: 10px;
+  }
+}
+
+.button--delete {
+  display: block;
+  margin: 0 auto;
+  background: red;
+}
+
+.button--remove {
+  background: none;
+  color: red;
+  text-transform: uppercase;
+  font-size: 11px;
+  align-self: flex-end;
+}
+
+ul {
+  display: block;
+  margin: 0 auto;
+  padding: 30px;
+  border: 1px solid rgba(0, 0, 0, 0.25);
+
+  > li {
+    color: $color-grey;
+    margin-bottom: 4px;
+  }
+}
+</style>
